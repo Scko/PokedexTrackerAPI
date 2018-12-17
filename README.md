373112cbe8f71758edbc015f929bb47634e20337
Scko <carl.lloyd@caresource.com>
2018-12-17 15:06:55 -0500

    Updated with more functions

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # PokedexTrackerAPI
+# To run: go build main.go
+
+#Look at this later: https://stackoverflow.com/questions/10838469/how-to-compile-go-program-consisting-of-multiple-files
--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"log"
 	"net/http"
 	"github.com/gorilla/mux"
+	"strconv"
 )
 
 type Pokemon struct{
@@ -21,11 +22,53 @@ func main(){
 	
 	router := mux.NewRouter()
 	
+	router.HandleFunc("/", DefaultRoute).Methods("GET")
 	router.HandleFunc("/pokemon", GetPokemon).Methods("GET")
+	router.HandleFunc("/pokemon/{id}", GetPokemonById).Methods("GET")
+	router.HandleFunc("/pokemon/{id}", CreatePokemon).Methods("POST")
+	router.HandleFunc("/pokemon/{id}", DeletePokemon).Methods("DELETE")
 	
 	log.Fatal(http.ListenAndServe(":8080", router))
 }
 
+func DefaultRoute(w http.ResponseWriter, r *http.Request){
+	json.NewEncoder(w).Encode("Hello World")
+}
+
 func GetPokemon(w http.ResponseWriter, r *http.Request){
 	json.NewEncoder(w).Encode(pokemon)
+}
+
+func GetPokemonById(w http.ResponseWriter, r *http.Request){
+	params := mux.Vars(r)
+	id, err := strconv.Atoi(params["id"])
+	if err != nil {
+		//handle this
+	}
+	id -= 1;
+	if(id < len(pokemon) && id >= 0){
+		json.NewEncoder(w).Encode(pokemon[id])
+	}else{
+		json.NewEncoder(w).Encode(Pokemon{})
+	}
+}
+
+func CreatePokemon(w http.ResponseWriter, r *http.Request){
+	params := mux.Vars(r)
+    var pkmn Pokemon
+    _ = json.NewDecoder(r.Body).Decode(&pkmn)
+    pkmn.ID = params["id"]
+    pokemon = append(pokemon, pkmn)
+    json.NewEncoder(w).Encode(pokemon)
+}
+
+func DeletePokemon(w http.ResponseWriter, r *http.Request){
+	params := mux.Vars(r)
+    for index, item := range pokemon {
+        if item.ID == params["id"] {
+            pokemon = append(pokemon[:index], pokemon[index+1:]...)
+            break
+        }
+        json.NewEncoder(w).Encode(pokemon)
+    }
 }
