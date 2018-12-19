86468f1edf2eff915b5d4f786848d811bbf059ae
Szko <carl.chester.lloyd@gmail.com>
2018-12-19 18:24:28 -0500

    Connect to pokeAPI

--- main.go
+++ main.go
@@ -6,7 +6,40 @@ import (
 	"net/http"
 	"github.com/gorilla/mux"
 	"strconv"
+	//"io/ioutil"
+	//"fmt"
 )
+
+type PokeAPI struct {
+	Count int
+	Next int
+	Previous int
+	Results []Result
+}
+
+type Result struct {
+	Name string
+	Url string
+}
+
+type Poke struct {
+	Id int
+	Name string
+	Height int
+	Weight int
+	Sprites []Sprite
+}
+
+type Sprite struct{
+	Back_Default string
+	Back_Female string
+	Back_Shiny string
+	Back_Shiny_Female string
+	Front_Default string
+	Front_Female string
+	Front_Shiny string
+	Front_Shiny_Female string
+}
 
 type Pokemon struct{
 	ID		string		`json:"id,omitempty"`
@@ -14,6 +47,7 @@ type Pokemon struct{
 	Number	string		`json:"number,omitempty"`
 }
 var pokemon []Pokemon
+var pokes []Poke
 
 func main(){
 
@@ -29,10 +63,37 @@ func main(){
 	router.HandleFunc("/pokemon/{id}", DeletePokemon).Methods("DELETE")
 	
 	log.Fatal(http.ListenAndServe(":8080", router))
+	
+	
 }
 
 func DefaultRoute(w http.ResponseWriter, r *http.Request){
-	json.NewEncoder(w).Encode("Hello World")
+	//Get all the pokemon urls
+	response, err := http.Get("https://pokeapi.co/api/v2/pokemon/")
+	if err != nil {
+		json.NewEncoder(w).Encode(err)
+	}
+	var data PokeAPI
+	if err := json.NewDecoder(response.Body).Decode(&data); err != nil {
+		log.Println(err)
+	}
+	//json.NewEncoder(w).Encode(data)
+	
+	//For each result make an individual request to get that pokemons data
+	for _, item := range data.Results{
+		response, err := http.Get(item.Url)
+		if err != nil {
+			json.NewEncoder(w).Encode(err)
+		}
+		var p Poke
+		if err := json.NewDecoder(response.Body).Decode(&p); err != nil {
+			log.Println(err)
+		}
+		pokes = append(pokes, p)
+	}
+	
+	//Return the data of all
+	json.NewEncoder(w).Encode(pokes)
 }
 
 func GetPokemon(w http.ResponseWriter, r *http.Request){
